60da83f341c865004c5b16d782836c4372b28ba9
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-11-08 16:23:45 -0700

    added login functionality with auth0. added routes and redux store

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from 'react-router-dom'
+import { Profile } from './Profile'
+import { Cart } from './Cart'
+import { Cookies } from './Cookies'
+
+const Home = () => {
+  const { loginWithRedirect, logout } = useAuth0();
+
+  return (
+    <>
+    <button onClick={() => loginWithRedirect()}>Log In</button>
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Log Out
+    </button>
+    <Link to='/cart'>Cart</Link>
+    <Profile />
+    <Cart />
+
+    <h1>Kelsey's Cookie Co.</h1>
+    <Cookies />
+    </>
+  )
+};
+
+export default Home

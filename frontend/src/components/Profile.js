ee0feec8f2d1b6f274a6ffb3aec73f55b7ff3e4a
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-11-08 18:31:49 -0700

    wrote profile component that renders after login

---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from 'axios'
+
+export const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isAuthenticated) {
+    var getToken = {
+      method: 'POST',
+      url: `https://${process.env.REACT_APP_DOMAIN}/oauth/token`,
+      headers: {'content-type': 'application/x-www-form-urlencoded'},
+      data: {
+        grant_type: 'client_credentials',
+        client_id: process.env.REACT_APP_CLIENT_ID,
+        client_secret: process.env.REACT_APP_CLIENT_SECRET,
+        audience: `https://${process.env.REACT_APP_DOMAIN}/api/v2/`
+      }
+    };
+  
+    let token;
+    axios.request(getToken).then(function (response) {
+      console.log(response.data);
+      token = response.data
+    }).catch(function (error) {
+      console.error(error);
+    });
+  
+    const getUser = {
+      method: 'PATCH',
+      url: `https://${process.env.REACT_APP_DOMAIN}/api/v2/users/${user.user_id}`,
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${token}`,
+        'cache-control': 'no-cache'
+      },
+      data: '{ "name": NAME_VALUE }'
+    };
+  
+    axios.request(getUser).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <h2>Welcome {user.given_name}!</h2>
+      </div>
+    )
+  );
+};
